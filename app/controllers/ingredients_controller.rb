4a3cb7be86d4fa64f1013d36a6dde7de957f783d
kotetsuCodes <kotetsuCodes@gmail.com>
2018-04-19 21:01:53 -0500

    added set_dish method to DRY it up

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,19 +1,17 @@
 class IngredientsController < ApplicationController
+  before_action :set_dish
 
   def index
-    @dish = Dish.find(params[:dish_id])
     json_response(@dish.ingredients)
   end
 
   def show
-    @dish = Dish.find(params[:dish_id])
     @ingredient = @dish.ingredients.find(params[:id])
 
     json_response(@ingredient)
   end
 
   def create
-    @dish = Dish.find(params[:dish_id])
     @ingredient = Ingredient.create!(ingredient_params)
 
     @dish.ingredients << @ingredient
@@ -22,14 +20,12 @@ class IngredientsController < ApplicationController
   end
 
   def destroy
-    @dish = Dish.find(params[:dish_id])
     @dish.ingredients.delete(Ingredient.find(params[:id]))
 
     head :no_content
   end
 
   def update
-    @dish = Dish.find(params[:dish_id])
     @ingredient = @dish.ingredients.find(params[:id])
 
     @ingredient.update(ingredient_params)
@@ -38,6 +34,10 @@ class IngredientsController < ApplicationController
   end
   
   private
+
+  def set_dish
+    @dish = Dish.find(params[:dish_id])
+  end
 
   def ingredient_params
     params.permit(:dish_id, :ingredient_id, :title, :quantity_name, :quantity_value)
